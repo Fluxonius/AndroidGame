08d6d5263e3d36d5a515bfd1d0c187968b561ec1
Fluxikus <leha.raf1999@gmail.com>
2019-06-12 22:24:43 +0200

    added sounds

---
+++ Assets/Scripts/MusicStop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStop : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public AudioClip background;
+    public AudioClip gameOverSound;
+    public AudioSource audioSource;
+    private bool konec;
+
+    void Start()
+    {
+        konec = false;
+        audioSource.clip = background;
+        audioSource.Play();
+        
+    }
+    void Update()
+    {
+        if (Scoring.gameOver)
+        {
+            audioSource.clip = gameOverSound;
+            audioSource.Play();
+            enabled = false;
+        }
+    }
+
+
+
+}
--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -7,27 +7,35 @@ public class Scoring : MonoBehaviour
     public static int score;
    // public GameObject restart;
     public static bool gameOver = false;
+   
    // Animator anim;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Portal")
-        {
-            if(!gameOver)score++;
-            Destroy(gameObject);
-            //restart.SetActive(true);
-        }
-        if (collision.gameObject.tag == "Saw")
-        {
-            //anim.SetTrigger("Death");
-            Destroy(gameObject);
-            gameOver = true;
+      
+            if (collision.gameObject.tag == "Portal")
+            {
+                if (!gameOver) score++;
+                Destroy(gameObject);
+                //restart.SetActive(true);
+            }
+            if (collision.gameObject.tag == "Saw")
+            {
+                //anim.SetTrigger("Death");
+                Destroy(gameObject);
+                gameOver = true;
+         
+
             
-        }
+           
+
+
+            }
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
